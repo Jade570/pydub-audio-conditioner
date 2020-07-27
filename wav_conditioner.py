74fb63314e909136987e4ad4d85e4b818a4e3da2
Jade570 <chaelyoung@naver.com>
2020-07-27 14:33:21 +0900

    Update wav_conditioner.py - add arguments parser

--- wav_conditioner.py
+++ wav_conditioner.py
@@ -1,6 +1,23 @@
 import os
+import sys
+import argparse
 import numpy as np
 from pydub import AudioSegment
+
+DIR = "../"
+SAMPLERATE = 24000
+BITRATE = 16
+SEC = 5000
+EXTENSION = "wav"
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Wav file conditioner for training dataset')
+    parser.add_argument('--dir', type=str, default=DIR, help='The directory you will store the conditioned files at. Default: '+DIR+'.')
+    parser.add_argument('--samplerate', type=int, default=SAMPLERATE, help='Sample rate you want to condition files as. Default: '+SAMPLERATE+'.')
+    parser.add_argument('--bitrate', type=int, default=BITRATE, help='Bit rate you want to condition files as. Defualt: '+BITRATE+'.')
+    parser.add_argument('--seconds', type=int, default=SEC, help='How many seconds you want to trim files as. Default: '+SEC+'.')
+    parser.add_argument('--extension', type=str, default=EXTENSION, help='Extension you want the conditioned files to have, Default :'+EXTENSION+'.')
+
 
 def downsample(input_wav, resample_sr): #write the samplerate you want to resample as
     audio = AudioSegment.from_wav(input_wav)
@@ -8,11 +25,14 @@ def downsample(input_wav, resample_sr): #write the samplerate you want to resamp
     return audio
 
 def splitsize(songnum, trimnum, audio, t1, size):
-    name = "../2008trim/"+str(songnum)+"-"+str(trimnum)+".wav"
+    _dir = args.dir
+    _extension = args.extension
+    _bitrate = args.bitrate
+    name = _dir+str(songnum)+"-"+str(trimnum)+"."+_extension
     t2 = t1 + size
     newAudio = audio
     newAudio = newAudio[t1:t2]
-    newAudio.export(name, format="wav")
+    newAudio.export(name, format=_extension, bitrate=_bitrate)
     t1 = t2
     return t1
 
@@ -20,14 +40,22 @@ def splitaudio(songnum, trimnum, audio):
     t1 = 0
     t2 = 0
     n = trimnum
-    while t1+5000 < len(audio): #while the file is not at the end
-        t1 = splitsize(songnum, n, audio, t1, 5000) #split by 5seconds
+    seconds = args.seconds
+    while t1+seconds < len(audio): #while the file is not at the end
+        t1 = splitsize(songnum, n, audio, t1, seconds)
         n += 1 
 
-song = 0
-for root, dirs, files in os.walk("."):
-    for filename in files:
-        if filename.endswith(".wav"):
-            splitaudio(song, 0, downsample(filename, 16000))
-            print(song)
-            song += 1
+def main():
+    song = 0
+    _samplerate = args.samplerate
+    _ext = args.extension
+    for root, dirs, files in os.walk("."):
+        for filename in files:
+            if filename.endswith("."+_ext):
+                splitaudio(song, 0, downsample(filename, _samplerate))
+                print(song)
+                song += 1
+
+
+if __name__ == '__main__':
+    main()
